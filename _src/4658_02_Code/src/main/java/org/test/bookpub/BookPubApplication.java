4a288e5ff0d4811e81640ec5f3b0afd39f0fcb7f
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-28 23:22:42 -0800

    I

---
+++ _src/4658_02_Code/src/main/java/org/test/bookpub/BookPubApplication.java
@@ -0,0 +1,23 @@
+package org.test.bookpub;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@EnableScheduling
+public class BookPubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPubApplication.class, args);
+    }
+
+    @Bean
+    public StartupRunner schedulerRunner() {
+        return new StartupRunner();
+    }
+}
